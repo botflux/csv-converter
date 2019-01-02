9f728d1b3fe06d88da0122d45d6a1cf1a39a5969
Victor Mendele <victor.mendele68@gmail.com>
2019-01-02 11:36:44 +0100

    feat: map

--- app.js
+++ app.js
@@ -1,7 +1,47 @@
 const fs = require ('fs')
 const csv = require('fast-csv')
+const inputFileName = 'csv/my.csv'
 
-const inputFileName = 'csv/my.min.csv'
+const transformDate = (date) => {
+    if (date.length !== 8) {
+        return "";
+    }
+
+    return `${date.slice(0, 4)}-${date.slice(4, 6)}-${date.slice(6)}`
+}
+
+const deceaseMap = {
+    root: 'Contenu',
+    rootEntity: 'Deces',
+    map: [
+        {
+            header: null,
+            default: 4,
+            newName: 'Act_Statut'
+        },
+        {
+            header: `numero d'acte`,
+            newName: 'Act_Numero'
+        },
+        {
+            header: `annee`,
+            newName: 'Act_Ordre'
+        },
+        {
+            header: `date de transcription`,
+            newName: 'Act_Date',
+            transform: transformDate
+        },
+        {
+            header: `heure`,
+            newName: 'Heure_Deces'
+        },
+        {
+            header: 'ville',
+            newName: 'Lie_Commune'
+        }
+    ]
+}
 
 let array = []
 
@@ -15,8 +55,22 @@ const stream = fs.createReadStream(inputFileName)
         headers: true,
     }))
     .on('data', data => {
-        //console.log(data)
-        array = [...array, ...[data]]
+        let o = {}
+
+        deceaseMap.map.forEach(el => {
+            let matching = data[el.header] || null
+
+            if (el.header === undefined || el.header === null) {
+                o = { ...o, ...{[el.newName]: el.default}}
+            }
+
+            if (matching !== null) {
+                let temp = { [el.newName]: (el.transform !== undefined && el.transform !== null) ? el.transform(matching) : matching }
+                o = {...o, ...temp }
+            } 
+        })
+
+        array = [...array, ...[o]]
     })
     .on('end', () => {
         //console.log('done')
@@ -26,6 +80,3 @@ const stream = fs.createReadStream(inputFileName)
             console.error (e)
         })
     })
-
-
-console.log(stream)
