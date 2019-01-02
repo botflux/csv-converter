dd3e77a0ab49e2878bbbb1cb58c73eb6df4a25e1
Victor Mendele <victor.mendele68@gmail.com>
2019-01-02 10:26:10 +0100

    Initial commit

---
+++ app.js
@@ -0,0 +1,29 @@
+const fs = require ('fs')
+const csv = require('fast-csv')
+
+let array = []
+
+/**
+ * Read the file stream and pipe it to a csv reader.
+ * All rows are read asynchronously.
+ */
+const stream = fs.createReadStream('csv/my.min.csv')
+    .pipe(csv({
+        delimiter: ';',
+        headers: true,
+    }))
+    .on('data', data => {
+        //console.log(data)
+        array = [...array, ...[data]]
+    })
+    .on('end', () => {
+        //console.log('done')
+        let json = JSON.stringify(array, null, 2)
+        console.log('done', json)
+        fs.writeFile('csv/obj.json', json, e => {
+            console.error (e)
+        })
+    })
+
+
+console.log(stream)
