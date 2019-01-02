87965c798f935c42a7fd988f4ffcab66e5be66ea
Victor Mendele <victor.mendele68@gmail.com>
2019-01-02 15:02:46 +0100

    feat: uniform resolver

--- app.js
+++ app.js
@@ -1,49 +1,8 @@
 const fs = require ('fs')
 const csv = require('fast-csv')
-const { deceaseMap } = require('./map.js')
+const { deceaseMap, resolveMap } = require('./map.js')
 const inputFileName = 'csv/my.csv'
 
-const transformDate = (date) => {
-    if (date.length !== 8) {
-        return "";
-    }
-
-    return `${date.slice(0, 4)}-${date.slice(4, 6)}-${date.slice(6)}`
-}
-/*
-const deceaseMap = {
-    root: 'Contenu',
-    rootEntity: 'Deces',
-    map: [
-        {
-            header: null,
-            default: 4,
-            newName: 'Act_Statut'
-        },
-        {
-            header: `numero d'acte`,
-            newName: 'Act_Numero'
-        },
-        {
-            header: `annee`,
-            newName: 'Act_Ordre'
-        },
-        {
-            header: `date de transcription`,
-            newName: 'Act_Date',
-            transform: transformDate
-        },
-        {
-            header: `heure`,
-            newName: 'Heure_Deces'
-        },
-        {
-            header: 'ville',
-            newName: 'Lie_Commune'
-        }
-    ]
-}
-*/
 let array = []
 
 /**
@@ -58,18 +17,7 @@ const stream = fs.createReadStream(inputFileName)
     .on('data', data => {
         let o = {}
 
-        deceaseMap.map.forEach(el => {
-            let matching = data[el.header] || null
-
-            if (el.header === undefined || el.header === null) {
-                o = { ...o, ...{[el.newName]: el.default}}
-            }
-
-            if (matching !== null) {
-                let temp = { [el.newName]: (el.transform !== undefined && el.transform !== null) ? el.transform(matching) : matching }
-                o = {...o, ...temp }
-            } 
-        })
+        o = resolveMap(deceaseMap, data)
 
         array = [...array, ...[o]]
     })
---
+++ map.js
@@ -0,0 +1,82 @@
+const { transformDate } = require ('./helpers.js')
+
+exports.deceaseMap = {
+    root: 'Deces',
+    map: [
+        {
+            header: null,
+            defaultValue: 4,
+            newName: 'Act_Statut',
+        },
+        {
+            header: `numero d'acte`,
+            newName: 'Act_Numero',
+        },
+        {
+            header: `annee`,
+            newName: 'Act_Ordre',
+        },
+        {
+            header: `date de transcription`,
+            newName: 'Act_Date',
+            resolveField: (data) => {
+                return `${data.slice(0, 4)}-${data.slice(4, 6)}-${data.slice(6)}`
+            }
+        },
+        {
+            header: `heure`,
+            newName: 'Heure_Deces',
+        },
+        {
+            header: 'ville',
+            newName: 'Lie_Commune',
+        },
+    ]
+}
+
+/**
+ * Returns the value inside the data object relative to the passed header
+ * @param {Object} mapField The map field associate
+ * @param {Object} data The object you want to map
+ */
+const getData = ({ header = null, defaultValue = "" }, data) => {
+    return data[header] || defaultValue
+}
+
+/**
+ * Returns a given value inside an object with the given newName associate
+ * @param {Object} mapField The map field
+ * @param {Object} value 
+ */
+const objectifyData = ({ defaultValue = "", newName = "Default" }, value) => {
+    return { [newName]: value || defaultValue }
+}
+
+/**
+ * Resolve an element
+ * @param {Object} el The map entry
+ * @param {Object} data The data that need to be resolve
+ */
+const resolveElement = (el, data) => {
+    const { resolveField = data => data } = el
+    return objectifyData(el, resolveField(getData (el, data)))
+}
+
+/**
+ * Resolve a row with a map
+ * @param {Object} mapObject The map matching your data 
+ * @param {Object} data Data represents an entry of your collection, for example with CSV data is a row
+ */
+const resolveMap = ({ root = "root", map = [] }, data) => {
+
+    let temp = {}
+
+    return map.reduce((previous, current) => {
+        return {...previous, ...resolveElement(current, data) }
+    }, {})
+}
+
+
+
+exports.resolveElement = resolveElement
+exports.resolveMap = resolveMap
