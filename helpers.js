38be3e786824ef3e5d503cfa5dd111b3d572e6ae
Victor Mendele <victor.mendele68@gmail.com>
2019-01-03 10:04:56 +0100

    feat: you can specify is you want empty field to be removed

--- helpers.js
+++ helpers.js
@@ -6,4 +6,13 @@ const getFirstWord = (str) => {
     return str.split(" ")[0]
 }
 
-exports.getFirstWord = getFirstWord
+/**
+ * If the passed variable is null or empty returns true else false
+ * @param {*} v Field to test
+ */
+const isNullOrEmpty = (v) => {
+    return (v === undefined || v === null) || (v.toString !== undefined && v.toString().length === 0)
+}
+
+exports.getFirstWord        = getFirstWord
+exports.isNullOrEmpty       = isNullOrEmpty
--- map.js
+++ map.js
@@ -1,3 +1,5 @@
+const { isNullOrEmpty } = require('./helpers.js')
+
 /**
  * Returns the value inside the data object relative to the passed header
  * @param {Object} data The object you want to map
@@ -25,9 +27,14 @@ const objectifyData = ({ defaultValue = "", newName = "Default" }, value) => {
 const resolveElement = (el, data) => {
 
     // REFACTORISER POUR QUE L'ON ENVOIE TOUTES LES DONNEES A CHAQUE FOIS
-    const { resolveField = getData } = el
-    return objectifyData(el, resolveField(data, el))
-    //getData (el, data)
+    const { resolveField = getData, removeIfEmpty = false } = el
+    let resolved = resolveField(data, el)
+    
+    if (removeIfEmpty && isNullOrEmpty(resolved)) {
+        return {}
+    }
+    
+    return objectifyData(el, resolved)
 }
 
 /**
@@ -37,23 +44,30 @@ const resolveElement = (el, data) => {
  * @param {Object} data Data represents an entry of your collection, for example with CSV data is a row
  */
 const resolveMap = ({ map = [] }, data) => {
-    
     let keys = Object.keys(data)
-    return keys.reduce((previous, current) => {
+
+    // forEach key, we search for a map matching with this key
+    // if there is a map matcing this key then we resolve the data and return it
+    let reduced = keys.reduce((previous, current) => {
         let matchingMap = map.find(mapElement => {
             return mapElement.header === current
         })
         if (matchingMap !== undefined && matchingMap !== null) {
             return {...previous, ...resolveElement(matchingMap, data)}
-        } else {
-            return previous
         }
+
+        return previous
     }, {})
 
-    /*
-    return map.reduce((previous, current) => {
-        return {...previous, ...resolveElement(current, data) }
-    }, {})*/
+    // We filter all the mapElement without header (default value field hasn't header)
+    let headerlessMapElement = map.filter((mapElement) => mapElement.header === null)
+    // forEach element without header we resolve data and return it
+    let headerless = headerlessMapElement.reduce((previous, current) => {
+        return {...previous, ...resolveElement(current, data)}
+    }, {})
+
+    // we return the concatenation of the data reduced with a header and without a header 
+    return {...reduced, ...headerless}
 }
 
 exports.resolveElement = resolveElement
